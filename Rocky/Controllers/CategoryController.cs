53e2927a53283737930cb6e8c4d7370f2bdd76ea
Kevin <kevin.shaughnessy18@gmail.com>
2022-04-04 22:32:18 -0400

    You can now delete a category

--- Rocky/Controllers/CategoryController.cs
+++ Rocky/Controllers/CategoryController.cs
@@ -61,5 +61,53 @@ namespace Rocky.Controllers
 
             return View(obj);
         }
+
+        //POST - EDIT
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Category.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+        }
+
+        //Get - Delete
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Category.Find(id);
+            if (obj == null)
+            {
+                return NotFound(obj);
+            }
+
+            return View(obj);
+        }
+
+        //POST - Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Category.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+                _db.Category.Remove(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+ 
+
+        }
     }
 }
